fd95801633c7eb39f3d8f0f7a0e17f34efacd7fa
software-kraft <jose.garcia@timify.com>
2021-05-14 16:41:41 +0200

    update impressum text

--- src/components/impressum.jsx
+++ src/components/impressum.jsx
@@ -1,78 +1,108 @@
-import Modal from 'react-modal';
-import { useState } from 'react'
-
+import Modal from "react-modal";
+import { useState } from "react";
 
 export const Impressum = () => {
-  const [modalIsOpen,setIsOpen] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
   const customStyles = {
-    content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)'
-    }
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-40%",
+      transform: "translate(-50%, -50%)",
+    },
   };
   function openModal() {
     setIsOpen(true);
   }
-  
-  
-  function closeModal(){
+
+  function closeModal() {
     setIsOpen(false);
   }
 
   return (
     <div>
-            
-      <div className='container text-center'>
-      <a onClick={openModal}>Impressum</a>
+      <div className="container text-center">
+        <a href="#" onClick={openModal}>
+          Imprint
+        </a>
       </div>
 
-        <Modal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          style={customStyles}
-          contentLabel="Example Modal"
-        >
-        <p>Disclosures pursuant to § 5 TMG:
-
-        Software-kraft.com is run by the company: Jose Riquelme & Jakub Nieznalski UG
-        Director: Jose Riquelme 
-        Knotestraße 16
-        81479 Munich
-        </p>
-        <p>
-        Contact:
-        <br/>
-        Email: info&#123;at&#125;software-kraft.com
-        <br/>
-        Website: www.software-kraft.com
-        </p>
-
-        <p>
-        Entry in the Register:
-        German Commercial Register
-        Registry Court:  Amtsgericht Munich Register Number: HRB 265255
-        Tax ID No. pursuant to §27 a Value Added Tax Act: (in progress)
-        </p>
-        <p>
-        Website design: http://www.templatewire.com/
-        </p>
-        <p>
-        Information on online dispute resolution: <br/>
-
-        The European Commission maintains an online dispute resolution platform. This is a central contact point for consumers and entrepreneurs who wish to settle disputes, for example in connection with an online purchase contract, simply, efficiently and inexpensively out of court. The online dispute resolution platform is available at the following address:
-        
-        https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&lng=DE 
-
-        We do not participate in any online dispute resolution and are not required to do so.
-
-        For all links to other sites on the Internet, Software-kraft.com expressly declares that it has no influence on the design and content of the linked sites. Therefore, we hereby expressly distance ourselves from all contents of all linked third-party sites on software-kraft.com, do not adopt these contents as our own and do not assume any liability or guarantee for these third-party contents. This declaration applies to all displayed links and to all contents of the pages to which links lead.
-        </p>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Imprint"
+      >
+        <div onClick={closeModal}>
+          <p>
+            <strong>Details pursuant to § 5 TMG:</strong>
+            <br />
+            Jose Riquelme & Jakub Nieznalski UG <br />
+            Knotestraße 16
+            <br />
+            D-81479 Munich
+            <br />
+          </p>
+          <p>
+            <strong>Represented by:</strong>
+            <br />
+            Jose Riquelme
+          </p>
+          <p>
+            <strong>Contact:</strong>
+            <br />
+            Email: info&#123;at&#125;software-kraft.com
+            <br />
+            Website: www.software-kraft.com
+          </p>
+          <p>
+            <strong>Registry entry:</strong>
+            <br />
+            Entry in the Commercial Register.
+            <br /> Register of companies: HambMunichurg Local Court
+            (Amtsgericht)
+            <br />
+            Register No.: HRB 265255
+            <br />
+          </p>
+          <p>
+            <strong>VAT ID No.:</strong>
+            <br />
+            VAT ID No. pursuant to section 27 a of the German Turnover Tax Law
+            (UStG): <br />
+            (in progress)
+          </p>
+          <p>
+            <strong>Disclaimer</strong>
+            <br />
+            Information on online dispute resolution: <br />
+            The European Commission maintains an online dispute resolution
+            platform. This is a central contact point for consumers and
+            entrepreneurs who wish to settle disputes, for example in connection
+            with an online purchase contract, simply, efficiently and
+            inexpensively out of court. The online dispute resolution platform
+            is available at the following address:
+            <br />
+            https://ec.europa.eu/consumers/odr/main/index.cfm?event=main.home2.show&lng=DE
+            <br />
+            We do not participate in any online dispute resolution and are not
+            required to do so. For all links to other sites on the Internet,
+            woftware-kraft.com expressly declares that it has no influence on
+            the design and content of the linked sites. <br />
+            Therefore, we hereby expressly distance ourselves from all contents
+            of all linked third-party sites on software-kraft.com, do not adopt
+            these contents as our own and do not assume any liability or
+            guarantee for these third-party contents. This declaration applies
+            to all displayed links and to all contents of the pages to which
+            links lead.
+          </p>
+          <p>
+            <strong>Website design:</strong> http://www.templatewire.com/
+          </p>
+        </div>
       </Modal>
-
     </div>
-  )
-}
+  );
+};

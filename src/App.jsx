b1c6da410428fe0cc43bc3d99754df3b7e686b32
software-kraft <jose.garcia@timify.com>
2021-09-26 15:42:04 +0200

    Add cookie consent

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import { useState, useEffect } from 'react'
-import { Navigation } from './components/navigation'
-import { Header } from './components/header'
+import { useState, useEffect } from "react";
+import { Navigation } from "./components/navigation";
+import { Header } from "./components/header";
 //import { Features } from './components/features'
-import { About } from './components/about'
-import { Services } from './components/services'
+import { About } from "./components/about";
+import { Services } from "./components/services";
 //import { Gallery } from './components/gallery'
 //import { Testimonials } from './components/testimonials'
-import { Team } from './components/Team'
-import { Contact } from './components/contact'
-import JsonData from './data/data.json'
-import SmoothScroll from 'smooth-scroll'
+import { Team } from "./components/Team";
+import { Contact } from "./components/contact";
+import JsonData from "./data/data.json";
+import SmoothScroll from "smooth-scroll";
+import CookieConsent from "react-cookie-consent";
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
   speedAsDuration: true,
-})
+});
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({})
+  const [landingPageData, setLandingPageData] = useState({});
   useEffect(() => {
-    setLandingPageData(JsonData)
-  }, [])
+    setLandingPageData(JsonData);
+  }, []);
 
   return (
     <div>
@@ -35,8 +36,20 @@ const App = () => {
       */}
       <Team data={landingPageData.Team} />
       <Contact data={landingPageData.Contact} />
+      <CookieConsent
+        location="bottom"
+        buttonText="I agree"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: "#eee", color: "#000" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+      >
+        <span style={{ fontSize: 12 }}>
+          This website uses cookies to enhance the user experience.
+        </span>
+      </CookieConsent>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
